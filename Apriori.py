a70a6a782b922a37a7e2e6faa99a38834fff857b
Ryan L Buchanan <buchanan.ryan22@gmail.com>
2020-10-10 12:14:26 -0600

    Import libraries and dataset

--- Apriori.py
+++ Apriori.py
@@ -1,2 +1,10 @@
 # Apriori Algorithm tutorial from Machine Learning A-Z - SuperDataScience -> Input by Ryan L Buchanan 09OCT20
 
+# Import libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Import the dataset
+dataset = pd.read_csv('Mall_Customers.csv')
+X = dataset.iloc[:, [3,4]].values
