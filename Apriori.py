51c7bb893f382a41483d12977f00c152787e555c
Ryan L Buchanan <buchanan.ryan22@gmail.com>
2020-10-11 11:59:50 -0600

    Loaded dataset

--- Apriori.py
+++ Apriori.py
@@ -6,5 +6,6 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Import the dataset
-dataset = pd.read_csv('Mall_Customers.csv')
+dataset = pd.read_csv('Market_Basket_Optimisation.csv')
 X = dataset.iloc[:, [3,4]].values
+
