5e0274eacec4a5a23a182b0eb7c005ff61e72c00
Ryan L Buchanan <buchanan.ryan22@gmail.com>
2020-10-11 12:09:07 -0600

    Set up project in Spyder

--- Apriori.py
+++ Apriori.py
@@ -5,7 +5,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# Import the dataset
+# Data prerocessing
 dataset = pd.read_csv('Market_Basket_Optimisation.csv')
 X = dataset.iloc[:, [3,4]].values
+
+# Train the Apriori model on the dataset
+
+
+# Visualize the result
+
+
+# Display the first result coming directly from the output of the apriori function
+
+
+# Put the results into a well-organized Pandas DataFrame
+
+
+# Display the results non-sorted
+
+
+# Display the results sorted by descending lifts
 
