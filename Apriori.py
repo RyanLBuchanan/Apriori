9193ca41191845fd3e8774310e1fcd5526324b3e
Ryan L Buchanan <buchanan.ryan22@gmail.com>
2020-10-12 14:45:11 -0600

    Installed apyori package

--- Apriori.py
+++ Apriori.py
@@ -1,5 +1,7 @@
 # Apriori Algorithm tutorial from Machine Learning A-Z - SuperDataScience -> Input by Ryan L Buchanan 09OCT20
 
+!pip install apyori
+
 # Import libraries
 import numpy as np
 import matplotlib.pyplot as plt
