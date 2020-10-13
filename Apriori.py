9f1066d60e594a99b8936ec88082d8e99cba93f3
Ryan L Buchanan <buchanan.ryan22@gmail.com>
2020-10-12 18:09:53 -0600

    Rules of association set

--- Apriori.py
+++ Apriori.py
@@ -16,6 +16,8 @@ for i in range(0, 7501):
 
 # Train the Apriori model on the dataset
 from apyori import apriori
+rules = apriori(transactions, min_support = 0.003, min_confidence = 0.2, min_lift = 3, min_length = 2,
+                max_length = 2)
 
 # Visualize the result
 
