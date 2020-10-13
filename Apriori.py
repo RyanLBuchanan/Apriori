b285849b7129bdd56d2f866298b56ab035b35968
Ryan L Buchanan <buchanan.ryan22@gmail.com>
2020-10-12 18:25:28 -0600

    Added visualization table for rules

--- Apriori.py
+++ Apriori.py
@@ -26,7 +26,14 @@ results = list(rules)
 results
 
 # Put the results into a well-organized Pandas DataFrame
-
+def inspect(results):
+    lhs = [tuple(result[2][0][0])[0] for result in results]
+    rhs = [tuple(result[2][0][1])[0] for result in results]
+    supports = [result[1] for result in results]
+    confidence = [result[2][0][2] for result in results]
+    lifts = [result[2][0][3] for result in results]
+    return list(zip(lhs, rhs, supports, confidence, lifts))
+resultsinDataFrame = pd.DataFrame(inspect(results), columns = ['Left-hand Side', 'Right-hand Side', 'Support', 'Confidence', 'Lifts'])
 
 # Display the results non-sorted
 
